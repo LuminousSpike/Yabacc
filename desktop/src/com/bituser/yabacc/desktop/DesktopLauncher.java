087de951bc6ad2c9b2fd6936d8eae16e0aef7c89
Nathan Todd <nathan.j.todd@gmail.com>
2015-10-26 22:15:40 +1300

    Changed the desktop launcher settings

--- desktop/src/com/bituser/yabacc/desktop/DesktopLauncher.java
+++ desktop/src/com/bituser/yabacc/desktop/DesktopLauncher.java
@@ -8,5 +8,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Yabacc(), config);
+		config.vSyncEnabled = true; // Setting to false disables vertical sync
+		config.foregroundFPS = 0; // Setting to 0 disables foreground fps throttling
+		config.width = 1280;
+		config.height = 720;
 	}
 }
