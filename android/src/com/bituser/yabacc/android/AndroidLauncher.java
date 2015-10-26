6df1de2da8d9b3c73fd372e052e8ec24a8dd8a3d
Nathan Todd <nathan.j.todd@gmail.com>
2015-10-26 22:11:53 +1300

    Initial commit

---
+++ android/src/com/bituser/yabacc/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bituser.yabacc.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bituser.yabacc.Yabacc;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Yabacc(), config);
+	}
+}
---
+++ desktop/src/com/bituser/yabacc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bituser.yabacc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bituser.yabacc.Yabacc;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Yabacc(), config);
+	}
+}
