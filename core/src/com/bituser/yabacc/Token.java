c2cdef8365ddc868b29cfd093b8a54e85913a4f6
Nathan Todd <nathan.j.todd@gmail.com>
2015-11-16 15:56:37 +1300

    Fully ported over the Token Class

---
+++ core/src/com/bituser/yabacc/Token.java
@@ -0,0 +1,35 @@
+package com.bituser.yabacc;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2; 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Color;
+
+public class Token extends Entity {
+
+    public Token (float x, float y, Color color) {
+        super(x, y, 15, 15);
+        _color = color;
+    }
+
+    @Override
+    public void update (float deltaTime) {
+
+    }
+
+    @Override
+    public void render (ShapeRenderer shapeRenderer) {
+        shapeRenderer.setColor(_color);
+        shapeRenderer.rect(_rect.x, _rect.y, _width, _height);
+    }
+
+    @Override
+    public void render (SpriteBatch batch) {
+
+    }
+}
