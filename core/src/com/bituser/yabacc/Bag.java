cb814332d3d03823a04c31494d7a80c0d9efa2b0
Nathan Todd <nathan.j.todd@gmail.com>
2016-07-13 01:34:47 +1200

    Refactored imports for the Bag class

--- core/src/com/bituser/yabacc/Bag.java
+++ core/src/com/bituser/yabacc/Bag.java
@@ -1,18 +1,8 @@
 package com.bituser.yabacc;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
-import java.util.*;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Bag extends EntityCollection {
     BitmapFont _font;
