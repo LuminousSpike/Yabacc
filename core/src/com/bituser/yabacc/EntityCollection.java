0a8c4b54ff847068776c5a68ef977d607e630a4e
Nathan Todd <nathan.j.todd@gmail.com>
2016-07-13 01:32:43 +1200

    Refactored imports for the EntityCollection class

--- core/src/com/bituser/yabacc/EntityCollection.java
+++ core/src/com/bituser/yabacc/EntityCollection.java
@@ -1,18 +1,10 @@
 package com.bituser.yabacc;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
-import java.util.*;
 
 public abstract class EntityCollection extends Entity {
     List<Entity> _entities= new ArrayList<Entity>();
