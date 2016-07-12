8afced1e5455ba4b8d6af56259c96c9f997e571b
Nathan Todd <nathan.j.todd@gmail.com>
2016-07-13 01:11:30 +1200

    Refactored imports for the Yabacc class

--- core/src/com/bituser/yabacc/Yabacc.java
+++ core/src/com/bituser/yabacc/Yabacc.java
@@ -3,7 +3,6 @@ package com.bituser.yabacc;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.InputAdapter;
@@ -12,7 +11,6 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
-import com.badlogic.gdx.graphics.Color;
 
 public class Yabacc extends ApplicationAdapter {
 	SpriteBatch batch;
