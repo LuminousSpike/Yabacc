eff0e78ee94a568751fa18fb114ef35ab64440a0
Nathan Todd <nathan.j.todd@gmail.com>
2016-07-13 01:29:25 +1200

    Refactored imports for the Token class

--- core/src/com/bituser/yabacc/Token.java
+++ core/src/com/bituser/yabacc/Token.java
@@ -1,14 +1,9 @@
 package com.bituser.yabacc;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
 public class Token extends Entity {
 
