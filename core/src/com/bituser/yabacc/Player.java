fd461f7370a722fb36e437546e1e1a0c573fa644
Nathan Todd <nathan.j.todd@gmail.com>
2016-07-13 01:31:35 +1200

    Refactored imports for the Player class

--- core/src/com/bituser/yabacc/Player.java
+++ core/src/com/bituser/yabacc/Player.java
@@ -1,16 +1,11 @@
 package com.bituser.yabacc;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import java.util.ArrayList;
+
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import java.util.*;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
 public class Player extends Entity {
     protected Hand _hand, _trophyHand;
