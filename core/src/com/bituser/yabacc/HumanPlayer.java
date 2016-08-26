7e750f8b1faa8d2a6fac692ad680615180dc2c53
Nathan Todd <nathan.j.todd@gmail.com>
2016-08-26 20:32:25 +1200

    Refactored access modifiers for HumanPlayer

--- core/src/com/bituser/yabacc/HumanPlayer.java
+++ core/src/com/bituser/yabacc/HumanPlayer.java
@@ -5,12 +5,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 
-public class HumanPlayer extends Player {
-    Vector2 _mousePos = new Vector2();
+class HumanPlayer extends Player {
+    private Vector2 _mousePos = new Vector2();
 
-    Card _currentCard, _selectedCard;
+    private Card _currentCard, _selectedCard;
 
-    public HumanPlayer (float x, float y, Color color) {
+    HumanPlayer(float x, float y, Color color) {
         super(x, y, color);
     }
 
@@ -30,19 +30,19 @@ public class HumanPlayer extends Player {
         super.render(batch);
     }
 
-   public void touchDown (float x, float y, int pointer, int button) {
+   void touchDown(float x, float y, int pointer, int button) {
        _selectedCard = _hand.touchDown(x, y, pointer, button);
    }
 
-   public void touchUp (float x, float y, int pointer, int button) {
+   void touchUp(float x, float y, int pointer, int button) {
        _selectedCard = _hand.touchUp(x, y, pointer, button);
    }
 
-   public void mouseMoved (float x, float y) {
+   void mouseMoved(float x, float y) {
         _mousePos.set(x, y);
         _hand.mouseMoved(x, y);
      }
-    public void touchDragged (int x, int y, int pointer) {
+    void touchDragged(int x, int y, int pointer) {
         _hand.touchDragged(x, y, pointer);
     }
 }
