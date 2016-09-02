841d01a60236d5219fbe5f141c1a5f3a6612046a
Nathan Todd <nathan.j.todd@gmail.com>
2016-09-03 01:39:22 +1200

    Bag now uses the GenericCollection class

--- core/src/com/bituser/yabacc/Bag.java
+++ core/src/com/bituser/yabacc/Bag.java
@@ -3,14 +3,17 @@ package com.bituser.yabacc;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.RandomXS128;
+import com.badlogic.gdx.math.Vector2;
 
-public class Bag extends EntityCollection {
+public class Bag extends GenericCollection<Token> {
     BitmapFont _font;
+    RandomXS128 _rand = new RandomXS128();
 
     public Bag (float x, float y,
-            Color token1, Color token2, Color token3, Color token4,
+            Color token1, Color token2, Color  token3, Color token4,
             Color token5, BitmapFont font) {
-        super(x, y, 80, 80);
+        super(new Vector2(x, y), 80, 80);
         _font = font;
 
         createTokens(13, token1);
@@ -29,9 +32,7 @@ public class Bag extends EntityCollection {
         super.add(token);
     }
 
-    public Token get () {
-        return (Token) super.get();
-    }
+    protected Token get () { return getRandom(); }
 
     private void createTokens (int amount, Color tokenColor) {
         for (int i = 0; i < amount; i++) {
