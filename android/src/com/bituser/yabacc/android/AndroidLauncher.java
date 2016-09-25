8e9aa4d38ac2ff8020156a5736d650e41172f40d
Nathan Todd <nathan.j.todd@gmail.com>
2016-09-26 00:17:29 +1300

    Implement immersive mode on supported devices
    If the android device is running a high enough android version, the game
    will launch in immersive mode.

--- android/src/com/bituser/yabacc/android/AndroidLauncher.java
+++ android/src/com/bituser/yabacc/android/AndroidLauncher.java
@@ -1,6 +1,8 @@
 package com.bituser.yabacc.android;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -11,6 +13,12 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		// Enable immersive mode if we can
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		}
+
 		initialize(new Yabacc(), config);
 	}
 }
