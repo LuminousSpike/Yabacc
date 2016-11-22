de22230825a0cbbfbacdcc7e4ae0a9ff74bfc587
Nathan Todd <nathan.j.todd@gmail.com>
2016-11-22 17:34:17 +1300

    Created skeleton ComputerPlayer class

---
+++ core/src/com/bituser/yabacc/ComputerPlayer.java
@@ -0,0 +1,10 @@
+package com.bituser.yabacc;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+
+class ComputerPlayer extends Player {
+    ComputerPlayer (float x, float y, Color color) {
+        super(x, y, color);
+    }
+}
